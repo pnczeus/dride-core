99bd0ece16acc955700c2e39e85f4c7f90fd2511
Yossi <saoron@gmail.com>
2016-12-25 10:12:52 +0200

    wrap the shell in a function

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 var PythonShell = require('python-shell');
- 
-var options = {
-  mode: 'text'
-};
 
 
-PythonShell.run('core/Cardigan/mainPi.py', options, function (err, results) {
-  if (err) throw err;
-  // results is an array consisting of messages collected during execution 
-  console.log('results: %j', results);
-});
+var runDeamon = function () {
+
+
+	var options = {
+	  mode: 'text'
+	};
+
+
+	PythonShell.run('core/Cardigan/mainPi.py', options, function (err, results) {
+	  if (err) throw err;
+	  // results is an array consisting of messages collected during execution 
+	  console.log('results: %j', results);
+	});
+
+
+}
